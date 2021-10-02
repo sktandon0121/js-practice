cb47aa5d236f212dce8a1b6cc60da4d59c5822c1
Subodh Tandon <sktandon0121@gmail.com>
2021-10-03 03:05:50 +0530

    promises for all

---
+++ src/promises/promise_all.js
@@ -0,0 +1,19 @@
+
+function getAllPromiseData() {
+    let users = fetch("https://jsonplaceholder.typicode.com/users")
+    let products = fetch("https://jsonplaceholder.typicode.com/posts/1")
+
+    Promise.all([users, products])
+        .then(values => {
+            // console.log("promise data ", values)
+            return Promise.all(values.map(e => e.json()))
+            // return values.map(e => e.json())
+        })
+        .then(([users, products]) => {
+            console.log('products ', products)
+            console.log('users ', users)
+        })
+
+}
+
+export default getAllPromiseData
